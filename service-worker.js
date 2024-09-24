e318efb82efdcfcb54083016a8ccb07af6f72931
digiherbanusantara <imam.dhn13@gmail.com>
2024-09-24 21:29:41 +0700

    PWA 12 MAC

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,18 @@
+let basePath = window.location.hostname === 'localhost' ? '/' : '/omni/';
+
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('my-cache').then(cache => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/about.html',
-        '/login.html',
-        '/css/style.css',
-        '/js/main.js',
-        '/codcrm.html',
-        '/tfcrm.html',
-        '/images/pwa.png'
-        
+        `${basePath}`,
+        `${basePath}index.html`,
+        `${basePath}about.html`,
+        `${basePath}login.html`,
+        `${basePath}css/style.css`,
+        `${basePath}js/main.js`,
+        `${basePath}codcrm.html`,
+        `${basePath}tfcrm.html`,
+        `${basePath}images/pwa.png`
       ]);
     })
   );
